5be92ba8651ce6e0dae55fa4c94c8972d224941c
MmGrand <maksimzarubin20002000@gmail.com>
2024-04-21 13:19:44 +0700

    commit-2

---
+++ src/components/CardList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="p-10">
+    <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <Card
+        title="Мужские Кроссовки Nike Blazer Mid Suede"
+        image-url="/sneakers/sneakers-1.jpg"
+        :price="1200"
+        :is-added="true"
+				:is-favorite="true"
+      />
+    </div>
+  </div>
+</template>
