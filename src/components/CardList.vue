f0cacc6c606864a299285a83463aa48af3bb2de3
MmGrand <maksimzarubin20002000@gmail.com>
2024-04-21 22:01:27 +0700

    commit 3

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,18 +1,25 @@
 <script setup>
 import Card from './Card.vue'
+
+defineProps({
+  items: Array
+});
+
+const emit = defineEmits(['addToFavorite']);
 </script>
 
 <template>
-  <div class="p-10">
-    <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-    <div class="grid grid-cols-4 gap-5">
-      <Card
-        title="Мужские Кроссовки Nike Blazer Mid Suede"
-        image-url="/sneakers/sneakers-1.jpg"
-        :price="1200"
-        :is-added="true"
-				:is-favorite="true"
-      />
-    </div>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :image-url="item.imageUrl"
+      :price="item.price"
+      :is-added="false"
+      :isFavorite="item.isFavorite"
+      :onClickFavorite="() => emit('addToFavorite', item)"
+    />
   </div>
 </template>
