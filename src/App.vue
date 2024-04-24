cd6122d9de3ceabec1f7874afc2042e66ee4599f
Максим Зарубин <maksimzarubin20002000@gmail.com>
2024-04-24 10:37:57 +0700

    commit-7

--- src/App.vue
+++ src/App.vue
@@ -51,10 +51,10 @@ provide('cart', {
 <template>
   <Drawer v-if="drawerOpen" :total-price="totalPrice" :vat-price="vatPrice" />
 
-  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl min-h-screen my-14">
     <Header :total-price="totalPrice" @open-drawer="openDrawer" />
 
-    <div class="p-10 h-screen">
+    <div class="p-10">
       <router-view></router-view>
     </div>
   </div>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,9 @@
 import axios from 'axios'
 import debounce from 'lodash.debounce'
 import { inject, reactive, watch, ref, onMounted } from 'vue'
+
 import CardList from '../components/CardList.vue'
+import infoBlock from '../components/infoBlock.vue'
 
 const { cart, addToCart, removeFromCart } = inject('cart')
 
@@ -95,16 +97,13 @@ const fetchItems = async () => {
 
 onMounted(async () => {
   cart.value = JSON.parse(localStorage.getItem('cart') || '[]')
-
   await fetchItems()
   await fetchFavorites()
-
   items.value = items.value.map((item) => ({
     ...item,
     isAdded: cart.value.some((cartItem) => cartItem.id === item.id)
   }))
 })
-
 watch(cart, () => {
   items.value = items.value.map((item) => ({
     ...item,
@@ -116,33 +115,42 @@ watch(filters, fetchItems)
 </script>
 
 <template>
-  <div class="flex justify-between items-center">
-    <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+  <div v-if="items.length !== 0">
+    <div class="flex justify-between items-center">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
-    <div class="flex gap-4">
-      <select
-        @change="onChangeSelect"
-        class="py-2 px-3 border rounded-md outline-none bg-white"
-        name="sort"
-      >
-        <option value="title">По название</option>
-        <option value="price">По цене (дешевые)</option>
-        <option value="-price">По цене (дорогие)</option>
-      </select>
+      <div class="flex gap-4">
+        <select
+          @change="onChangeSelect"
+          class="py-2 px-3 border rounded-md outline-none bg-white"
+          name="sort"
+        >
+          <option value="title">По название</option>
+          <option value="price">По цене (дешевые)</option>
+          <option value="-price">По цене (дорогие)</option>
+        </select>
 
-      <div class="relative">
-        <img class="absolute left-4 top-3" src="/search.svg" alt="Поиск" />
-        <input
-          @input="onChangeSearchInput"
-          class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
-          type="text"
-          placeholder="Поиск..."
-        />
+        <div class="relative">
+          <img class="absolute left-4 top-3" src="/search.svg" alt="Поиск" />
+          <input
+            @input="onChangeSearchInput"
+            class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+            type="text"
+            placeholder="Поиск..."
+          />
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="mt-10">
-    <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus" />
+    <div class="mt-10">
+      <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus" />
+    </div>
+  </div>
+  <div class="mt-60" v-else>
+    <infoBlock
+      title="Товаров нет :("
+      description="К сожалению, в магазине сейчас нет товаров. Приходите позже."
+      image-url="/emoji-2.png"
+    />
   </div>
 </template>
